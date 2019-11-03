b6bedef9afb6ade18831c5ebff49bb66c2686554
Adrián Poza <adripoza23@gmail.com>
2019-11-03 02:48:25 +0100

    lectura de preguntas en json y niveles de dificultad

--- src/main/java/com/urjc/quizgame/GameActivity.java
+++ src/main/java/com/urjc/quizgame/GameActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Debug;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -13,13 +14,27 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.ImageView;
 import android.widget.Toast;
+import android.util.Log;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
+
 
 public class GameActivity extends AppCompatActivity {
 
     private int score;
+    private int MAX_QUESTIONS;
     private ArrayList<Question> questions = new ArrayList<>();
     private RadioGroup radioGroup;
     private RadioButton radioBtn1;
@@ -40,69 +55,9 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        //Configuración de las preguntas
+        MAX_QUESTIONS = (getSharedPreferences("MyPrefsFile", 0).getInt("difficulty", 1) +1)* 6;
 
-        Question q1, q2, q3, q4, q5;
-        q1 = q2 = q3 = q4 = q5 = new Question();
-
-        switch(getIntent().getStringExtra("MODE")) {
-            case "Música":
-                String a1m[] = {"Mozart", "Vivaldi", "Beethoven", "Hans Zimmer"};
-                q1 = new Question(0, "¿Quién compuso esta canción?", new ArrayList<>(Arrays.asList(a1m)), 1, null, "estaciones");
-
-                String a2m[] = {"Nueva York", "Philadelphia", "Chicago", "Nueva Orleans"};
-                q2 = new Question(1, "¿En qué ciudad se originó el Jazz?", new ArrayList<>(Arrays.asList(a2m)), 3);
-
-                String a3m[] = {"4", "2", "8", "3"};
-                q3 = new Question(2, "¿A cuántas semicorcheas equivale una blanca?", new ArrayList<>(Arrays.asList(a3m)), 2);
-
-                String a4m[] = {"tuba", "trombón", "saxofón", "trompeta"};
-                q4 = new Question(3, "¿Qué instrumento es este?", new ArrayList<>(Arrays.asList(a4m)), 0, "tuba");
-
-                String a5m[] = {"saxofón", "trompeta", "flauta travesera", "fagot"};
-                q5 = new Question(4, "¿Cuál de estos instrumentos no es de viento-madera?", new ArrayList<>(Arrays.asList(a5m)), 1);
-                break;
-            case"Cine":
-                String a1c[] = {"Champiñón", "Calavera", "Caimán", "Prohibida"};
-                q1 = new Question(0, "¿Cómo se llama la isla en la que vive King Kong?", new ArrayList<>(Arrays.asList(a1c)), 1);
-
-                String a2c[] = {"coche", "barco", "tren", "avión"};
-                q2 = new Question(1, "¿Qué transporte es popular por ser filmado por los hermanos Lumière?", new ArrayList<>(Arrays.asList(a2c)), 2);
-
-                String a3c[] = {"Shrek", "Monsters Inc.", "Toy Story", "El Viaje de Chihiro"};
-                q3 = new Question(2, "¿Cual de estas ganó el primer Óscar a mejor película de animación?", new ArrayList<>(Arrays.asList(a3c)), 0);
-
-                String a4c[] = {"Teresa", "Amalia", "Ofelia", "Yolanda"};
-                q4 = new Question(3, "¿Cual era el nombre de la protagonista del Laberinto del Fauno?", new ArrayList<>(Arrays.asList(a4c)), 2);
-
-                String a5c[] = {"Paprika", "Perfect Blue", "Tokyo Godfathers", "La Tumba de las Luciérnagas"};
-                q5 = new Question(4, "¿Cual de estas películas no pertenece a Satoshi Kon?", new ArrayList<>(Arrays.asList(a5c)), 3);
-                break;
-            case "Historia":
-                String a1h[] = {"1512", "1502", "1482", "1492"};
-                q1 = new Question(0, "En qué año descubrió Colón América", new ArrayList<>(Arrays.asList(a1h)), 3);
-
-                String a2h[] = {"Champiñón", "Cerdeña", "Córcega", "Santa Elena"};
-                q2 = new Question(1, "¿En qué Isla murió Napoleón?", new ArrayList<>(Arrays.asList(a2h)), 3);
-
-                String a3h[] = {"Quito", "Cuzco", "Machu Pichu", "Lima"};
-                q3 = new Question(2, "¿Cuál era la capital del imperio Inca?", new ArrayList<>(Arrays.asList(a3h)), 1);
-
-                String a4h[] = {"1812", "1820", "1814", "1802"};
-                q4 = new Question(3, "De qué año es la primera Constitución española?", new ArrayList<>(Arrays.asList(a4h)), 0);
-
-                String a5h[] = {"griegos y espartacos", "romanos y cartagineses", "griegos y persas", "griegos y egipcios"};
-                q5 = new Question(4, "¿Quiénes lucharon en la batalla de la maraton?", new ArrayList<>(Arrays.asList(a5h)), 2);
-                break;
-        }
-
-        questions.add(q1);
-        questions.add(q2);
-        questions.add(q3);
-        questions.add(q4);
-        questions.add(q5);
-
-
+        readQuestions(getIntent().getStringExtra("MODE"));
 
         //COnfiguración RadioButtons
 
@@ -117,17 +72,17 @@ public class GameActivity extends AppCompatActivity {
 
         });
 
-        radioBtn1 = (RadioButton) findViewById(R.id.radioBtn1);
-        radioBtn2 = (RadioButton) findViewById(R.id.radioBtn2);
-        radioBtn3 = (RadioButton) findViewById(R.id.radioBtn3);
-        radioBtn4 = (RadioButton) findViewById(R.id.radioBtn4);
-        question = (TextView) findViewById(R.id.question);
-        questionCount = (TextView) findViewById(R.id.textQuestionCount);
-        scoreText= (TextView)findViewById(R.id.textScore);
+        radioBtn1 = findViewById(R.id.radioBtn1);
+        radioBtn2 = findViewById(R.id.radioBtn2);
+        radioBtn3 = findViewById(R.id.radioBtn3);
+        radioBtn4 = findViewById(R.id.radioBtn4);
+        question = findViewById(R.id.question);
+        questionCount = findViewById(R.id.textQuestionCount);
+        scoreText= findViewById(R.id.textScore);
         score = 0;
 
-        chooseBtn = (Button)findViewById(R.id.chooseBtn);
-        imageView = (ImageView)findViewById(R.id.imageView);
+        chooseBtn = findViewById(R.id.chooseBtn);
+        imageView = findViewById(R.id.imageView);
 
         chooseBtn.setOnClickListener(new View.OnClickListener() {
 
@@ -184,8 +139,8 @@ public class GameActivity extends AppCompatActivity {
                 }
                 else{
                     //Siguiente pregunta
-                    if(currentQuestion.getId() <= questions.size()-2)
-                        changeQuestion(currentQuestion.getId() + 1);
+                    if(currentQuestion.getId() <= questions.size()-2 && currentQuestion.getId() <= MAX_QUESTIONS)
+                        changeQuestion (currentQuestion.getId() + 1);
                     else{
                         Intent intent = new Intent(v.getContext(), ScoreActivity.class);
                         intent.putExtra("SCORE", score+"");
@@ -214,14 +169,21 @@ public class GameActivity extends AppCompatActivity {
 
         questionCount.setText("Question " + (currentQuestion.getId() + 1) + "/" + questions.size());
 
-        int img = getResources().getIdentifier("@drawable/"+currentQuestion.getImageId(),null,this.getPackageName());
-        imageView.setImageResource(img);
+        Log.d("Debug", "imageId: " + currentQuestion.getImageId());
+        Log.d("Debug", "audioId: " + currentQuestion.getMusicId());
+
+        if(!currentQuestion.getImageId().equals("null") && currentQuestion.getImageId()!= null) {
+            int img = getResources().getIdentifier("@drawable/" + currentQuestion.getImageId(), null, this.getPackageName());
+            imageView.setImageResource(img);
+        }else{
+            imageView.setImageResource(0);
+        }
 
         //Detiene la reproducción de audio de la pregunta anterior
         stop();
         //Reproduce el nuevo audio en caso de que exista
-        if (currentQuestion.getMusicId() != null){
-            play();
+        if (!currentQuestion.getMusicId().equals("null") && currentQuestion.getMusicId() != null){
+            //play();
         }
 
         radioBtn1.setText(currentQuestion.getOption(0));
@@ -238,7 +200,7 @@ public class GameActivity extends AppCompatActivity {
     //Crea un nuevo reproductor en caso de que no exista y lo reproduce
     private void play(){
         if (player == null){
-            int music = getResources().getIdentifier("@raw/"+currentQuestion.getMusicId(),null,this.getPackageName());
+            int music = getResources().getIdentifier("@raw/"+currentQuestion.getMusicId(),null, this.getPackageName());
             player = MediaPlayer.create(this, music);
         }
         player.start();
@@ -249,6 +211,52 @@ public class GameActivity extends AppCompatActivity {
         if (player != null){
             player.release();
             player = null;
+        }
+    }
+
+    private void readQuestions(String type){
+        try {
+
+            InputStream recursoRaw;
+            if(type.equals("Música"))
+                recursoRaw = getResources().openRawResource(R.raw.musica);
+            else
+                recursoRaw = getResources().openRawResource(R.raw.videojuegos);
+            byte[] b = new byte[recursoRaw.available()];
+            recursoRaw.read(b);
+
+            String s = new String(b);
+            JSONObject obj = new JSONObject(new String(b));
+
+            JSONArray array = obj.getJSONArray("music");
+
+            for(int i = 0 ; i < array.length() ; i++){
+
+                Question question = new Question();
+                JSONObject q = array.getJSONObject(i);
+                question.setQuestion(q.getString("question"));
+                JSONArray answers = q.getJSONArray("answers");
+                ArrayList<String> list = new ArrayList<>();
+
+                for(int j = 0; j < answers.length(); j++){
+                    list.add(answers.get(j).toString());
+                }
+
+                question.setOptions(list);
+                question.setAnswer(q.getInt("correct"));
+                question.setId(i);
+                question.setImageId(q.getString("image"));
+                question.setMusicId(q.getString("audio"));
+
+                questions.add(question);
+            }
+
+        }
+        catch(IOException exc){
+            Log.d("Error", exc.toString());
+        }
+        catch(JSONException exc){
+            Log.d("Error", exc.toString());
         }
     }
 }
